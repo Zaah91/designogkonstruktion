5e80e5991e73f5eaa03b846dc9543e510d97e218
David Praëm Andreassen <124776389+BPraem@users.noreply.github.com>
2024-11-06 15:33:44 +0100

    Forum first version

---
+++ src/components/ChatWindow.vue
@@ -0,0 +1,64 @@
+<!-- SectionContent.vue -->
+<template>
+  <section class="WindowSection" v-if="section">
+    <div class="WindowSectionMain">
+      <h2>{{ section.mainTitle }}</h2>
+      <div class="TextSection">
+        <h3>{{ section.title }}</h3>
+        <p>{{ section.text1 }}</p>
+        <p>{{ section.text2 }}</p>
+        <button class="Button" @click.prevent>Join Forum</button>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "ChatWindow",
+  props: {
+    section: Object,
+  },
+};
+</script>
+
+<style scoped>
+.WindowSection {
+  background-color: #ad5950;
+  position: absolute;
+  right: 0;
+  bottom: 0;
+  top: 80px;
+  width: 64%;
+  padding: 20px;
+  border-radius: 14px;
+}
+.WindowSectionMain{
+  width: 100%;
+  height: 100%;
+  background-color: #ffffff;
+  padding: 30px;
+  border-radius: 14px;
+}
+.WindowSectionMain h2{
+  font-size: 28px;
+  margin-bottom: 18px;
+}
+.TextSection > * {
+  margin-bottom: 16px;
+  width: 80%;
+}
+.TextSection h3{
+  font-size: 24px;
+}
+.TextSection p{
+  font-size: 20px;
+}
+.Button{
+  padding: 12px;
+  background-color: #f17c6f;
+  margin: 20px;
+  border-radius: 18px;
+  width: 160px;
+}
+</style>
---
+++ src/components/ForumSection.vue
@@ -0,0 +1,70 @@
+<!-- SectionSelector.vue -->
+<template>
+  <nav class="ForumSectionNav">
+    <button
+      :class="{ active: selectedSection === 'sectionOne' }"
+      class="NavButtonOne NavButton"      
+      @click="selectSection('sectionOne')"
+    >
+    Svømmeklubben Ishøj
+    </button>
+    <button
+      :class="{ active: selectedSection === 'sectionTwo' }"
+      class="NavButton"
+      @click="selectSection('sectionTwo')"
+    >
+      Bage Klubben
+    </button>
+    <button
+      :class="{ active: selectedSection === 'sectionThree' }"
+      class="NavButtonThree NavButton"      
+      @click="selectSection('sectionThree')"
+    >
+    Velkommen til Gå-klubben Amager
+    </button>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "ForumSection",
+  data() {
+    return {
+      selectedSection: 'sectionOne', 
+    };
+  },
+  methods: {
+    selectSection(section) {
+      this.selectedSection = section; 
+      this.$emit("selectSection", section);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.ForumSectionNav {
+  display: flex;
+  justify-content: end;
+  flex-wrap: wrap;
+  font-size: 18px;
+  padding: 24px;
+}
+.NavButton {
+  background-color: #f17c6f;
+  padding: 18px;
+  font-size: 18px;
+}
+.NavButtonOne{
+  border-top-left-radius: 8px; 
+}
+.NavButtonThree{
+  border-top-right-radius: 8px;
+}
+.NavButton:hover {
+  background-color: #fa8073;
+}
+.NavButton.active {
+  background-color: #AD5950;
+}
+</style>
