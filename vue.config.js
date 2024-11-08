f0ccebe49717aea6ffcb36bf26927dffe694af33
Jacob <jacobseated@gmail.com>
2024-11-08 10:12:04 +0100

    Switch to relative paths for easier deployment

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: './' // Når vi deploy'er appen skal vi bruge relative stier, fordi ellers kan vi ikke placere appen i en subfolder på vores domæne. Denne indstilling slår det til for os.
 })
