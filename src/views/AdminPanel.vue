e2d096c8440c6c2a8c53ab09d670c9f69bebb3b7
Martin Rohde <zaah91@gmail.com>
2024-11-15 11:38:03 +0100

    AdminPanel file made

---
+++ src/views/AdminPanel.vue
@@ -0,0 +1,6 @@
+<!--
+Copilot:
+I need an admin, it need the following: a overview of all users. A search function to find a specifik user. When pressing a user, i need it to make a box on the side, where i can change info about the user, abd the possibility to delete the user. The database is not made yet.
+
+Might work as a starting point:
+-->
