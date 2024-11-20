828e092ed329c26db7cd99dbcd14085d8c306273
Jacob <jacobseated@gmail.com>
2024-11-20 15:55:10 +0100

    Fix: Property isLoggedIn was accessed during render but is not defined on instance.

--- src/components/LogIn.vue
+++ src/components/LogIn.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container v-if="!isLoggedIn" fluid class="bgrnd fill-height d-flex justify-center align-center">
+  <v-container fluid class="bgrnd fill-height d-flex justify-center align-center">
     <v-card class="pa-5 text-center" max-width="400" outlined>
       <h1 class="text-h4 mb-5">Velkommen til Gammelchat</h1>
       
