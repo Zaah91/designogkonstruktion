1887a2b1b516d82a7f0a31b2d593cfbb995e6903
Jacob <jacobseated@gmail.com>
2024-12-04 01:09:28 +0100

    Support page now shows up again

--- src/views/SupportView.vue
+++ src/views/SupportView.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-main v-if="this.siteInfo.loggedIn" :class="{ mainContent: this.siteInfo.loggedIn }">
+  <v-main v-if="loggedInUser" :class="{ mainContent: loggedInUser }">
     <div>
       <v-container>
         <h1>Support</h1>
@@ -8,33 +8,26 @@
       <v-container class="pa-4">
         <p>Har du spørgsmål eller brug for hjælp?</p>
         <p>Ring på tlf: <b>123 123 11</b></p>
-      </v-container>
 
-      <v-table  class="pa-4 text-left">
-          <th>
-            Åbningstider
-          </th>
-          <tr
-            v-for="item in openings"
-            :key="item.name"
-          >
+        <h2>Åbningstider</h2>
+        <v-table class="py-4 text-left">
+          <tr v-for="item in openings" :key="item.name">
             <td>{{ item.name }}</td>
             <td>{{ item.time }}</td>
           </tr>
-      </v-table>
+        </v-table>
 
-      <v-container class="pa-4">
         <p>Eller send os en besked:</p>
-        
+
         <v-alert v-if="successMessage" type="success" class="mt-4">
-            {{ successMessage }}
+          {{ successMessage }}
         </v-alert>
 
         <v-form v-model="isValid" @submit.prevent="handleSubmit">
           <v-text-field
             v-model="form.title"
             label="Emne"
-            :rules="[v => !!v || 'Emne mangler']"
+            :rules="[(v) => !!v || 'Emne mangler']"
             outlined
             dense
             required
@@ -43,17 +36,20 @@
           <v-textarea
             v-model="form.description"
             label="Beskrivelse"
-            :rules="[v => !!v || 'Beskrivelse mangler']"
+            :rules="[(v) => !!v || 'Beskrivelse mangler']"
             outlined
             dense
             required
           ></v-textarea>
 
-          <v-btn :disabled="!isValid" color="primary" type="submit" class="mt-4">
+          <v-btn
+            :disabled="!isValid"
+            color="primary"
+            type="submit"
+            class="mt-4"
+          >
             Submit
           </v-btn>
-
-          
         </v-form>
       </v-container>
     </div>
@@ -61,6 +57,7 @@
 </template>
 
 <script>
+import { useLoggedInUserStore } from "../stores/loggedInUser";
 
 export default {
   name: "SupportView",
@@ -69,25 +66,29 @@ export default {
     return {
       isValid: false, // Tracks form validity
       form: {
-        title: '',
-        description: ''
+        title: "",
+        description: "",
       },
-      successMessage: '', // Success message after submission
-      
+      successMessage: "", // Success message after submission
+
       openings: [
-        { name: 'Mandag til fredag', time: "10:00 - 14:00" },
+        { name: "Mandag til fredag", time: "10:00 - 14:00" },
         //{ name: 'Tirsdag', time: "10:00 - 14:00" },
         //{ name: 'Onsdag', time: "10:00 - 14:00" },
         //{ name: 'Torsdag', time: "10:00 - 14:00" },
         //{ name: 'Fredag', time: "10:00 - 14:00" },
-        { name: 'Lørdag og søndag', time: "Lukket" },
+        { name: "Lørdag og søndag", time: "Lukket" },
         //{ name: 'Søndag', time: "Lukket" },
-      ]
+      ],
     };
   },
   computed: {
     username() {
-      return this.siteInfo.username || 'User';
+      return this.siteInfo.username || "User";
+    },
+    loggedInUser() {
+      // Retuner user-objektet for den bruger, som er logget ind
+      return useLoggedInUserStore().user;
     },
   },
   methods: {
@@ -96,19 +97,19 @@ export default {
         // Simulate form submission
 
         // Show a success message
-        this.successMessage = 'Your message has been submitted!';
-        
+        this.successMessage = "Your message has been submitted!";
+
         // Clear form fields
-        this.form.title = '';
-        this.form.description = '';
+        this.form.title = "";
+        this.form.description = "";
         this.isValid = false;
 
         // Clear the success message after 10 seconds
         setTimeout(() => {
-          this.successMessage = '';
+          this.successMessage = "";
         }, 10000);
       }
-    }
-  }
+    },
+  },
 };
 </script>
